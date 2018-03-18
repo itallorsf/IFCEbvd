f0800b76409a2afd6e321cacee0b29a0d18a5ff1
itallorsf <itallorsf@outlook.com>
2018-03-18 00:18:47 -0300

    Modificado para funcionar no sistema do IFSP

--- ifspbvd.py
+++ ifspbvd.py
@@ -1,29 +1,64 @@
 # -*- coding: utf-8 -*-
 #    _ ____       Instituto Federal de
 #   (_) __/        Educação, Ciência e
-#  / / _/          Tecnologia do Ceará
+#  / / _/          Tecnologia de São Paulo
 # /_/_/  BIBLIOTECA VIRTUAL DOWNLOADER
 
 from selenium import webdriver
 import urllib, md5, os, sys
+from selenium.webdriver import Firefox
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.wait import WebDriverWait
 
 def _baixa(_url,_nome):
   try:
     urllib.urlretrieve(_url, _nome)
   except:
-    print 'problemas ao baixar %s' % _nome
+    print 'Problemas ao baixar %s' % _nome
   else:
     print '%s baixado' % _nome
 
 _hash = lambda(_mat): 'login=%s&token=%s' % (_mat, md5.new('%sQJEkJM2iLJiAj6LScxsZivml54SmzSy0' % _mat).hexdigest())
 
-def _dump(matricula, id_livro):
-  b=webdriver.PhantomJS()
-  print 'gerando cookie de login para matricula %s...' % matricula
-  b.get('http://ifce.bv3.digitalpages.com.br/user_session/authentication_gateway?%s' % _hash(matricula))
-  print 'inicializando...'
+def _dump(matricula, senha, id_livro):
+  options = Options()
+  #options.add_argument('--headless')
+  options.add_argument('--disable-gpu')
+  driver = Firefox(executable_path='geckodriver', firefox_options=options) 
+  b=driver
+ 
+  try:
+	  print 'Acessando o SUAP...'
+	  b.get('https://suap.ifsp.edu.br/accounts/login/')
+	
+	  print 'SUAP acessado. \nFazendo login...'
+	
+	  b.find_element_by_id('id_username').send_keys(matricula)
+	  b.find_element_by_id('id_password').send_keys(senha)
+	  b.find_element_by_css_selector('body.login .submit-row input[type="submit"]').click()
+	
+	  b.implicitly_wait(2)
+		
+	  try:
+		  b.find_element_by_class_name('errornote')
+		  sys.exit("ERRO. Login ou senha inválidos!")
+	  except:
+		   print 'Login realizado com sucesso!'
+  except Exception as e: print(e)
+  
+  print "Acessando biblioteca virtual..."
+  try:
+	  b.find_element_by_partial_link_text('Biblioteca Virtual Pearson').click()
+	  print("Biblioteca acessada.")
+  except Exception as e: print(e)
+    
+  print 'Gerando cookie de login para matricula %s...' % matricula
+  b.get('http://ifsp.bv3.digitalpages.com.br/user_session/authentication_gateway?%s' % _hash(matricula))
+  print 'Inicializando...'
   b.get('http://ifce.bv3.digitalpages.com.br/users/publications/%s' % id_livro)
-  print 'obtendo informacoes para o livro %s...' % id_livro
+  print 'Obtendo informacoes para o livro %s...' % id_livro
   p_1 = 0
   while(p_1 == 0):
     try:
@@ -67,22 +102,24 @@ def _dump(matricula, id_livro):
 
 def _gerapdf(_livro):
   # usando a ferramenta convert do ImageMagick
-  print 'convertendo para PDF...'
+  print 'Convertendo para PDF...'
   os.system('convert *.jpg %s.pdf' % _livro)
 
 def _remove():
-  print 'limpando os jpgs residuais...'
-  os.system('rm *.jpg')
+  print 'Limpando os jpgs residuais...'
+  os.system('del *.jpg')
 
 if __name__ == "__main__":
-  if len(sys.argv) != 3:
-    print "\033[91mifcebvd.py -- IFCE Biblioteca Virtual Downloader  \033[0m"
-    print "\033[91msintaxe: ./ifcebvd.py [matricula] [id do livro]   \033[0m"
+  if len(sys.argv) != 4:
+    print "IFSP Biblioteca Virtual Pearson Downloader"
+    print "Sintaxe: ./ifspbvd.py [matricula] [senha] [id do livro]"
     sys.exit(1)
   else:
+    print "Iniciando... aguarde..."
     matricula = sys.argv[1]
-    livro = sys.argv[2]
-    _dump(matricula , livro)
+    senha - sys.argv[2]
+    livro = sys.argv[3]
+    _dump(matricula , matricula, livro)
     _gerapdf(livro)
     _remove()
-    print '\033[92moperacao finalizada.\033[0m'
+    print '\033[92mFinalizado. Livro convertido em PDF com sucesso!. Ate mais! :) \033[0m'
